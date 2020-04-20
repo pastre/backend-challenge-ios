773fe1ef9ec16cbc970db659b625416de18261d5
Bruno Pastre <pastre68@gmail.com>
2020-04-20 17:34:16 -0300

    Initial Commit

---
+++ ReflectionsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReflectionsApp
+//
+//  Created by Bruno Pastre on 20/04/20.
+//  Copyright © 2020 Bruno Pastre. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
